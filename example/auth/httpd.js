b0680fe06da2171e036580c7c7b0e3387b4d23f8
t-k- <clock126@126.com>
2017-07-07 21:15:14 +0800

    Modulized auth for express.js

--- example/auth/httpd.js
+++ example/auth/httpd.js
@@ -1,54 +1,19 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var auth= require('../../auth.js');
-
+var expAuth = require('../../express-auth.js');
 var app = express();
-app.use(bodyParser.json()); /* for req.body */
-app.use(cookieParser()); /* for req.cookies */
 
-function require_auth(req, res, next) {
-	var token = req.headers['tk-auth'] ||
-	            req.cookies['tk-auth'] ||
-	            '';
-	var authRes = auth.tokVerify(token);
-	var fromUrl = encodeURI(req.url);
+expAuth.init(app, {
+	keyName: 'tk-auth',
+	loginRoute: '/login'
+});
 
-	if (authRes.pass) {
-		return next();
-	} else {
-		res.redirect('/auth.html?next=' +
-		             encodeURIComponent(fromUrl));
-	}
-}
+app.get('/login', function (req, res) {
+	res.sendFile(__dirname + '/login.html');
 
-app.post('/login', function (req, res) {
-	var reqJson = req.body;
-	var ip = req.headers['x-real-ip'] ?
-	         req.headers['x-real-ip'] :
-	         req.ip;
+}).post('/login_auth', function (req, res) {
+	expAuth.handleLoginReq(req, res);
 
-	var loginRes = auth.login(
-		reqJson.username,
-		reqJson.password,
-		ip
-	);
-
-	res.cookie('tk-auth', loginRes.token, {
-		maxAge: 24 * 3600 * 1000, /* one day */
-		httpOnly: false /* prohibit js access to
-		                   this cookie */
-	});
-
-	console.log(reqJson.username + ': ' + loginRes.msg);
-	console.log(loginRes.perm);
-	console.log(loginRes.token);
-	res.json({
-		"loginPass": loginRes.pass,
-		"token": loginRes.token
-	});
-
-}).get('/private', require_auth, function (req, res) {;
+}).get('/private', expAuth.middleware, function (req, res) {
 	res.send('look at me!');
 });
 
---
+++ express-auth.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var auth= require('./auth.js');
+
+var options = {
+	keyName: 'auth',
+	loginRoute: '/login',
+	maxAge: 24 * 3600
+};
+
+exports.init = function (app, opt) {
+	app.use(bodyParser.json()); /* for req.body */
+	app.use(cookieParser()); /* for req.cookies */
+
+	['keyName', 'loginRoute', 'maxAge'].forEach( function (k) {
+		options[k] = opt[k] || options[k];
+	});
+}
+
+exports.middleware = function (req, res, next) {
+	var token = req.cookies[options['keyName']] || '';
+	var authRes = auth.tokVerify(token);
+	var fromUrl = encodeURI(req.url);
+
+	if (authRes.pass) {
+		return next();
+	} else {
+		res.redirect(options['loginRoute'] + '?next=' +
+		             encodeURIComponent(fromUrl));
+	}
+}
+
+exports.handleLoginReq = function (req, res) {
+	var reqJson = req.body;
+	var ip = req.headers['x-real-ip'] ?
+	         req.headers['x-real-ip'] :
+	         req.ip;
+
+	var loginRes = auth.login(
+		reqJson.username,
+		reqJson.password,
+		ip
+	);
+
+	res.cookie(options['keyName'], loginRes.token, {
+		maxAge: options['maxAge'] * 1000,
+		httpOnly: false /* prohibit js access to
+		                   this cookie */
+	});
+
+	console.log(reqJson.username + ': ' + loginRes.msg);
+	console.log(loginRes.perm);
+	console.log(loginRes.token);
+	res.json({
+		"loginPass": loginRes.pass,
+		"token": loginRes.token
+	});
+}
